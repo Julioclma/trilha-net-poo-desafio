bf8a1ed4061ee63bca07b245a7dce86d3327fc79
Julio Mafra <julioclmafra@gmail.com>
2025-07-31 00:39:04 -0300

    finalizando

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone = new Iphone("123456", "a7", "123456789", 120);
+
+iphone.InstalarAplicativo("whatsapp");
+
+Smartphone nokia = new Nokia("123456", "a7", "123456789", 120);
+
+nokia.InstalarAplicativo("whatsapp");
